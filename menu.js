dba0a63d05bf5bf0ef4ce6275cd88fef2ff2de30
Caio Farias <caio53901846@aluno.ms.senac.br>
2026-04-28 11:21:19 -0400

    atualização

--- menu.js
+++ menu.js
@@ -1,22 +1,17 @@
 function Idade(){
     let idade = Number (document.getElementById("idade").value);
 
-    if(idade >=2){
-        document.getElementById("msg").innerText = "Bebê"
-    }
-    else if(idade >=11){
-        document.getElementById("msg").innerText = "Criança"
-    }
-    else if(idade >=21){
-        document.getElementById("msg").innerText = "Jovem"
-    }
-    else if(idade >=64){
-        document.getElementById("msg").innerText = "Adulto"
-    }
-    else if(idade >=100){
-        document.getElementById("msg").innerText = "Idoso"
-    }
-    else if(idade <101){
-        document.getElementById("msg").innerText = "Rainha Elizabeth"
+    if (idade >= 101) {
+        msg.innerText = "Rainha Elizabeth"
+    } else if (idade >= 64) {
+        msg.innerText = "Idoso"
+    } else if (idade >= 25) {
+        msg.innerText = "Adulto"
+    } else if (idade >= 15) {
+        msg.innerText = "Jovem"
+    } else if (idade >= 4) {
+        msg.innerText = "Criança"
+    } else {
+         msg.innerText = "Bebê"
     }
 }
